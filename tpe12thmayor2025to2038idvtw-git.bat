4a32dee789a25907a6a37452506e27fb445715a4
goldshoot0720 <goldshoot0720@gmail.com>
2025-08-27 02:34:37 +0800

    20250827 v1

---
+++ tpe12thmayor2025to2038idvtw-git.bat
@@ -0,0 +1,8 @@
+cd "C:\Users\chbon\Documents\tpe12thmayor2025to2038idvtw"
+git init
+git add .
+git commit -m "20250827 v1"
+git remote add origin https://github.com/goldshoot0720/tpe12thmayor2025to2038idvtw.git
+git checkout -b main
+git push -u origin main
+pause
---
+++ tpe12thmayor2025to2038idvtw-start.bat
@@ -0,0 +1,4 @@
+cd "C:\Users\chbon\Documents\tpe12thmayor2025to2038idvtw"
+explorer http://localhost:3000
+npm run dev
+pause
