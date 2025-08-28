0479ea166c52dd47c82b4f783af8db61d274b6cc
goldshoot0720 <goldshoot0720@gmail.com>
2025-08-29 02:11:07 +0800

    20250829 v1

--- tpe12thmayor2025to2038idvtw-git.bat
+++ tpe12thmayor2025to2038idvtw-git.bat
@@ -1,8 +1,9 @@
-cd "C:\Users\chbon\Documents\tpe12thmayor2025to2038idvtw"
+cd "C:\Users\chbon\Documents\tpe12thmayor2025to2038idvtw_azure_devops_repos"
 git init
 git add .
-git commit -m "20250828 v6"
-git remote add origin https://github.com/goldshoot0720/tpe12thmayor2025to2038idvtw.git
-git checkout -b main
-git push -u origin main
+git commit -m "20250829 v1"
+git remote add origin https://chbondg@dev.azure.com/chbondg/tpe12thmayor2025to2038idvtw/_git/tpe12thmayor2025to2038idvtw.git
+git checkout -b all
+git push -u origin --all
 pause
+git pull origin main --rebase
