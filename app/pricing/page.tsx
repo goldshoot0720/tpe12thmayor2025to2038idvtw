ae36865adf5fda4396291c82063eb5dcb1684e1b
goldshoot0720 <goldshoot0720@gmail.com>
2025-08-30 13:59:13 +0800

    20250830 v1

--- app/pricing/page.tsx
+++ app/pricing/page.tsx
@@ -1,9 +1,49 @@
+"use client";
 import { title } from "@/components/primitives";
+import {
+  Card,
+  CardHeader,
+  CardBody,
+  CardFooter,
+  Divider,
+  Link,
+  Image,
+} from "@heroui/react";
 
 export default function PricingPage() {
   return (
     <div>
       <h1 className={title()}>Pricing</h1>
+      <Card className="max-w-[400px]">
+        <CardHeader className="flex gap-3">
+          <Image
+            alt="heroui logo"
+            height={40}
+            radius="sm"
+            src="https://avatars.githubusercontent.com/u/86160567?s=200&v=4"
+            width={40}
+          />
+          <div className="flex flex-col">
+            <p className="text-md">Coolify</p>
+            <p className="text-small text-default-500">coolify.io</p>
+          </div>
+        </CardHeader>
+        <Divider />
+        <CardBody>
+          <p>
+            An open-source & self-hostable Heroku / Netlify / Vercel
+            alternative.
+            <br />
+            鋒兄 GitHub Sponsors $5.
+          </p>
+        </CardBody>
+        <Divider />
+        <CardFooter>
+          <Link isExternal showAnchorIcon href="https://coolify.io/">
+            Visit Coolify.
+          </Link>
+        </CardFooter>
+      </Card>
     </div>
   );
 }
