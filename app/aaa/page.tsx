00fca1c2b5b1cc3375826601325a25b49445ec70
goldshoot0720 <goldshoot0720@gmail.com>
2025-09-03 00:36:24 +0800

    20250903 v1

---
+++ app/aaa/page.tsx
@@ -0,0 +1,121 @@
+"use client";
+import { title } from "@/components/primitives";
+import { Accordion, AccordionItem } from "@heroui/react";
+
+export default function AAAPage() {
+  return (
+    <div>
+      <h1 className={title()}>AAA</h1>
+      <Accordion isCompact>
+        <AccordionItem key="1" aria-label="Accordion 1" title="sjes8460105✅">
+          {"gmail/hotmail/github"}
+        </AccordionItem>
+        <AccordionItem key="2" aria-label="Accordion 2" title="dsjhs9031206✅">
+          {"gmail/hotmail/github"}
+        </AccordionItem>
+        <AccordionItem key="3" aria-label="Accordion 3" title="clhs9331143✅">
+          {"gmail/hotmail/github"}
+        </AccordionItem>
+        <AccordionItem key="5" aria-label="Accordion 5" title="cycu9627132✅">
+          {"manus"}
+        </AccordionItem>
+        <AccordionItem key="6" aria-label="Accordion 6" title="">
+          {""}
+        </AccordionItem>
+        <AccordionItem key="7" aria-label="Accordion 7" title="">
+          {""}
+        </AccordionItem>
+        <AccordionItem key="8" aria-label="Accordion 8" title="">
+          {""}
+        </AccordionItem>
+        <AccordionItem key="9" aria-label="Accordion 9" title="">
+          {""}
+        </AccordionItem>
+        <AccordionItem key="10" aria-label="Accordion 10" title="">
+          {""}
+        </AccordionItem>
+        <AccordionItem key="11" aria-label="Accordion 11" title="">
+          {""}
+        </AccordionItem>
+        <AccordionItem key="12" aria-label="Accordion 12" title="">
+          {""}
+        </AccordionItem>
+        <AccordionItem key="13" aria-label="Accordion 13" title="">
+          {""}
+        </AccordionItem>
+        <AccordionItem key="14" aria-label="Accordion 14" title="">
+          {""}
+        </AccordionItem>
+        <AccordionItem key="15" aria-label="Accordion 15" title="">
+          {""}
+        </AccordionItem>
+        <AccordionItem key="16" aria-label="Accordion 16" title="">
+          {""}
+        </AccordionItem>
+        <AccordionItem key="17" aria-label="Accordion 17" title="">
+          {""}
+        </AccordionItem>
+        <AccordionItem key="18" aria-label="Accordion 18" title="">
+          {""}
+        </AccordionItem>
+        <AccordionItem key="19" aria-label="Accordion 19" title="">
+          {""}
+        </AccordionItem>
+        <AccordionItem key="20" aria-label="Accordion 20" title="">
+          {""}
+        </AccordionItem>
+        <AccordionItem key="21" aria-label="Accordion 21" title="">
+          {""}
+        </AccordionItem>
+        <AccordionItem key="22" aria-label="Accordion 22" title="">
+          {""}
+        </AccordionItem>
+        <AccordionItem key="23" aria-label="Accordion 23" title="">
+          {""}
+        </AccordionItem>
+        <AccordionItem key="24" aria-label="Accordion 24" title="">
+          {""}
+        </AccordionItem>
+        <AccordionItem key="25" aria-label="Accordion 25" title="">
+          {""}
+        </AccordionItem>
+        <AccordionItem key="26" aria-label="Accordion 26" title="">
+          {""}
+        </AccordionItem>
+        <AccordionItem key="27" aria-label="Accordion 27" title="">
+          {""}
+        </AccordionItem>
+        <AccordionItem key="28" aria-label="Accordion 28" title="">
+          {""}
+        </AccordionItem>
+        <AccordionItem key="29" aria-label="Accordion 29" title="">
+          {""}
+        </AccordionItem>
+        <AccordionItem key="30" aria-label="Accordion 30" title="">
+          {""}
+        </AccordionItem>
+        <AccordionItem key="31" aria-label="Accordion 31" title="">
+          {""}
+        </AccordionItem>
+        <AccordionItem key="32" aria-label="Accordion 32" title="">
+          {""}
+        </AccordionItem>
+        <AccordionItem key="33" aria-label="Accordion 33" title="">
+          {""}
+        </AccordionItem>
+        <AccordionItem key="34" aria-label="Accordion 34" title="">
+          {""}
+        </AccordionItem>
+        <AccordionItem key="35" aria-label="Accordion 35" title="">
+          {""}
+        </AccordionItem>
+        <AccordionItem key="36" aria-label="Accordion 36" title="">
+          {""}
+        </AccordionItem>
+        <AccordionItem key="37" aria-label="Accordion 37" title="">
+          {""}
+        </AccordionItem>
+      </Accordion>
+    </div>
+  );
+}
